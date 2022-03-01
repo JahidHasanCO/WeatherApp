409dbffd3c10e37a122bb32900395298e16951b4
Jahid Hasan <vdjsovaj@gmail.com>
2022-03-01 10:28:07 +0600

    Update

--- app/src/main/java/com/example/weatherapp/presentation/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/weatherapp/presentation/fragment/HomeFragment.kt
@@ -1,24 +1,27 @@
 package com.example.weatherapp.presentation.fragment
 
+import android.content.Context
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.viewModels
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.weatherapp.R
 import com.example.weatherapp.Utils.ApiState
 import com.example.weatherapp.databinding.FragmentHomeBinding
-import com.example.weatherapp.databinding.FragmentSplashScreenBinding
 import com.example.weatherapp.presentation.adapter.CityAdapter
 import com.example.weatherapp.presentation.viewmodel.CityViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
+
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -27,6 +30,11 @@ class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var cityAdapter: CityAdapter
 
+
+
+
+    var lat = 0.0
+    var lon = 0.0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -83,5 +91,7 @@ class HomeFragment : Fragment() {
             adapter=cityAdapter
         }
     }
+
+
 
 }
